d05b45f50d5c761e49019efbe3b945cf99df7eb5
Shivam Vishwakarma <shivamvishwakarma2211@gmail.com>
2026-03-14 14:25:36 +0530

    adminPanelActive

--- backend/makeAdmin.js
+++ backend/makeAdmin.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('./src/models/User');
 
-mongoose.connect('mongodb://localhost:27017/jd-dishing-works').then(() => {
+mongoose.connect('mongodb+srv://officework2023abs_db_user:1mwVliKA0wxjLmxX@cluster0.97dj40u.mongodb.net/?appName=Cluster0').then(() => {
     return mongoose.connection.db.collection('users').updateOne(
         { email: 'test3@test.com' },
         { $set: { role: 'admin' } }
