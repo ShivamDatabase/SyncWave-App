02aef9a29f4c8de234bb5d0b9d8af0d14469de41
Shivam Vishwakarma <shivamvishwakarma2211@gmail.com>
2026-03-14 14:42:39 +0530

    specific user to an admin role in the database.

--- backend/makeAdmin.js
+++ backend/makeAdmin.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('./src/models/User');
 
-mongoose.connect('mongodb+srv://officework2023abs_db_user:1mwVliKA0wxjLmxX@cluster0.97dj40u.mongodb.net/?appName=Cluster0').then(() => {
+mongoose.connect('mongodb+srv://officework2023abs_db_user:1mwVliKA0wxjLmxX@cluster0.97dj40u.mongodb.net/test?appName=Cluster0').then(() => {
     return mongoose.connection.db.collection('users').updateOne(
         { email: 'test3@test.com' },
         { $set: { role: 'admin' } }
